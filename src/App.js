d8309a00a316acff71b6acd86c2edc8bef74dc1b
LapertJoseph <lapertjoseph@gmail.com>
2022-01-07 17:46:34 +0100

    premier composant

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
+import Item from './Item'
 
-
-function App() {            // Ceci est un composant ( une fonction) 
+function App() {                                      // Ceci est un composant ( une fonction) 
   return (
-    <div className="App">                           {/* On écris className au lieu de class car c'est un mot réservé */}
+    <div className="App">                           
       <h1>Hello app React</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
-      <input type="text" /> <br />                  {/* pour les inputs il faut les fermer sinon on aura une erreur */}
-      {5 + 5} <br />                                {/* on peut directement faire des calcul dans le html */}
-      {true ? 1000 : "abc"}                         {/* Rendu conditionel */}
+      <Item />                                        {/* on place Item ici en pensant à bien refermer la balise sinon erreur */}
     </div>
   );
 }
 
-export default App;       // export de la fonction App qui est retourner dans la div root 
+export default App;                                   // export de la fonction App qui est retourner dans la div root 
---
+++ src/Item.js
@@ -0,0 +1,10 @@
+function Item () {
+
+    return <h1>Hello world depuis item</h1>     // quand il y a une seule ligne de code on peux tout mettre sur une ligne sans les paranthèses
+    
+}
+
+export default Item;
+
+// "Composant Stateless" car il ne fournit qu'une présentation (il sert seulement a retourner un h1)
+// "Composant State" qui vont avoir des données 
