93dadc8ca21c33ba7603356ad73efa9d47315623
LapertJoseph <lapertjoseph@gmail.com>
2022-01-07 17:23:32 +0100

    Découverte JSX et ses normes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 
+
 function App() {            // Ceci est un composant ( une fonction) 
   return (
-    <div className="App">
+    <div className="App">                           {/* On écris className au lieu de class car c'est un mot réservé */}
       <h1>Hello app React</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <input type="text" /> <br />                  {/* pour les inputs il faut les fermer sinon on aura une erreur */}
+      {5 + 5} <br />                                {/* on peut directement faire des calcul dans le html */}
+      {true ? 1000 : "abc"}                         {/* Rendu conditionel */}
     </div>
   );
 }
