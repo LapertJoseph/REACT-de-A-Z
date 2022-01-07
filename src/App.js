aebf9039d44836bf5ad14774d8937c3453f2679e
LapertJoseph <lapertjoseph@gmail.com>
2022-01-07 18:06:35 +0100

    découverte useState()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import {useState} from 'react'
 import Item from './Item'
 
 function App() {                                      // Ceci est un composant ( une fonction) 
+
+  // console.log(useState(10));                          // quand on passe un argument a la méthode useState() ca va être la donnée de départ (tableau indice 0)
+
+  const [monState, setMonState] = useState(10)          // syntaxe d'un useState qui retourne un tableau  (State de départ, fonction qui modifie le state)
+  
+  console.log(monState);
+  console.log(setMonState);
+
   return (
     <div className="App">                           
       <h1>Hello app React</h1>
