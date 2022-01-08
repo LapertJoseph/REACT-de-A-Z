56a8582bd32a327c17daf14d948272524213b52f
LapertJoseph <lapertjoseph@gmail.com>
2022-01-08 21:01:29 +0100

    Ajout props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-
+import Item from './Item'
 function App() {                                      // Ceci est un composant ( une fonction) 
 
   // console.log(useState(10));                          // quand on passe un argument a la méthode useState() ca va être la donnée de départ (tableau indice 0)
@@ -12,11 +12,15 @@ function App() {                                      // Ceci est un composant (
 
   return (
     <div className="App">                           
-      <h1>Hello State : {monState}</h1>   {/* <= donnée du composant App (valeur de base dans le useState) */}
+      <h1>Hello State : {monState}</h1>                       {/* <= donnée du composant App (valeur de base dans le useState) */}
                                                
       <button onClick={modifyState}>Change State</button>
+      {/* <Item txt="Hello World"/>                           {/* On utilise un composant mais on peut lui attribuer différent props */}
+      {/* <Item txt="Hello Italy"/> */}
+      {/* <Item txt="Hello Japan"/> */}
+      <Item number={monState}/>                            {/* on peut aussi afficher la valeur du state de App */}
     </div>
   );
 }
 
-export default App;                                   // export de la fonction App qui est retourner dans la div root 
+export default App;                                              // export de la fonction App qui est retourner dans la div root 
--- src/Item.js
+++ src/Item.js
@@ -1,6 +1,9 @@
-function Item () {
+function Item (props) {
+    console.log(props);                         // props renvoi un objet et il va contenir ce qu'un parent lui donne
 
-    return <h1>Hello world depuis item</h1>     // quand il y a une seule ligne de code on peux tout mettre sur une ligne sans les paranthèses
+    // return <h1>{props.txt}</h1>         // props.txt renvoi le string passer dans app car app est son parent
+    return <h1>{props.number}</h1>      // props.number renvoi le state passer dans app car app est son parent
+                                        
     
 }
 
@@ -9,3 +12,7 @@ export default Item;
 // "Composant Stateless" car il ne fournit qu'une présentation (il sert seulement a retourner un h1)
 // "Composant State" qui vont avoir des données
 
+// seconde régle de rerender si on passe une props, le composant va se mettre a jour également.
+// exemple avec props.txt et props.number 
+
+// le premier renvois un texte et le second renvois un nombre sachant que tous les deux vont rerender le composant car le parent se rerender
